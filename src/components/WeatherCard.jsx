97eb3f7111300ba2ef2bb5331dd5f804c7ea32dc
Bublyk Svitlana <lirifailon06@gmail.com>
2025-10-15 17:04:26 +0300

    absrkjnwr

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -22,7 +22,9 @@ export default function WeatherCard({
             <h3>{dayName}</h3>
             <img src={icon} alt={condition} />
             <p className="condition">{condition}</p>
-            <p className="temp"><strong>Температура:</strong> {Math.round(temp_max)}°C / {Math.round(temp_min)}°C</p>
+            <p className="temp">
+                {Math.round(temp_max)}°C / {Math.round(temp_min)}°C
+            </p>
             <p className="details"><strong>Вологість:</strong> {humidity}%</p>
             <p className="details"><strong>Вітер:</strong> {wind} км/год</p>
             <p className="details"><strong>Ймовірність дощу:</strong> {chance_of_rain}%</p>
